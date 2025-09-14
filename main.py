b2ba17b252c540ed97710b20f5cdf9d8ea7445a4
hassanrajput7626 <hassanrajput7626@gmail.com>
2025-09-15 03:35:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # app.py
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_from_directory
 import requests
 import os
 
 app = Flask(__name__)
 
+# --------- Facebook Profile Fetch ------------
 def get_profile_name(access_token):
     url = "https://graph.facebook.com/me"
     params = {'access_token': access_token}
@@ -14,6 +15,12 @@ def get_profile_name(access_token):
         return data['name']
     return None
 
+# --------- Serve Local Videos ----------------
+@app.route("/videos/<path:filename>")
+def serve_video(filename):
+    return send_from_directory("static/videos", filename)
+
+# --------- Index Page ------------------------
 @app.route('/', methods=['GET', 'POST'])
 def index():
     profile_name = None
@@ -27,6 +34,7 @@ def index():
 
     return render_template('index.html', profile_name=profile_name, error_message=error_message)
 
+# --------- Run Server ------------------------
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
